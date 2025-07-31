a39effd2d0667d1f39f656a4dc9c9f859d62078c
Rakshak-ai <rakshakaisos@gmail.com>
2025-07-31 14:30:17 +0530

    Update MainActivity.java

--- app/src/main/java/com/rakshak/ai/MainActivity.java
+++ app/src/main/java/com/rakshak/ai/MainActivity.java
@@ -1,1 +1,16 @@
+package com.rakshak;
 
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txt = findViewById(R.id.mainText);
+        txt.setText("RakshakAI is Active");
+    }
+}
